00a7320aa6ef824a3eae4dbe36ac81b5eeb57d2d
AngelinaLagunova <lagunova_gelya@mail.ru>
2024-06-07 09:01:36 +1000

    new file:   src/data.js 	modified:   src/index.css     modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import text from './data.js'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+const Text = (props) => {
+
+  const [show, setShow] = React.useState([]);
+
+  const showAll = (event) =>{
+    let id = Number(event.target.id[1]);
+
+    setShow(show => [...show,id]);
+
+  }
+
+  const showLess = (event) =>{
+      let id = Number(event.target.id[1]);
+      
+      setShow(show => show.filter(item => item !== id));
+      
+  }
+
+  const print = props.data.map((item,index) =>
+    <p>
+      <span className={(show.indexOf(index)!==-1)? "Less": "More"}>
+
+          <span>{item.split(".")[0]}</span>
+          <span id={"d"+index} onClick={showAll}>...</span>
+
+      </span>
+
+      <span id={"p"+index} className={(show.indexOf(index)!==-1)? "More": "Less"} onClick={showLess}>
+
+        {item}
+
+      </span>
+
+    </p>
+  )
+
+  return(
+    <p>
+      {print}
+    </p>
+
+  )
+
+
+}
+
+
+
+function Content() { 
+  return(     
+    <>         
+      <h3><Text data={text}/></h3>     
+    </>     
+  ) 
+} 
+
+root.render(<Content />)
+
